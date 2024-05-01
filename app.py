b2870113f36e8a64260c6d10c6a2105f9dcca5b0
PrinceNanaPoku <princenanapoku64@gmail.com>
2024-05-01 18:07:04 +0000

    further commit

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 num1 = int(input('Enter first number:'));
 num2 = int(input('Enter second number:'));
 op = input('Enter operator:');
-if op == '+':
-    print(num1 + num2);
-elif op == '-':
-    print(num1 - num2);
-elif op == '*':
-    print(num1 * num2);
-else :
-    print(num1 / num2); 
+
+try:
+    if op == '+':
+        print(num1 + num2);
+    elif op == '-':
+        print(num1 - num2);
+    elif op == '*':
+        print(num1 * num2);
+    else :
+        print(num1 / num2); 
+except:
+    print('Error occcurred. Try again');
